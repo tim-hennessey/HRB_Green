f616dc12576f1b60a3a7007f58a8a2cdde291fd0
tim.hennessey <Tim.Hennessey@deutschinc.com>
2019-10-15 16:02:46 -0700

    Creative Update

---
+++ app/300x600_V3/scripts/Animation.js
@@ -0,0 +1,45 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+	var banner = document.getElementById('banner');
+	var t = TweenMax;
+	var tl = new TimelineMax();
+	var txt2a = document.getElementById('txt2a');
+	var txt2b = document.getElementById('txt2b');
+	var txt2c = document.getElementById('txt2c');
+
+	// --------------------------------------------------------------------------------------
+	// set default properties
+	function initialize() {
+		// DO NOT EDIT: reveals banner once loaded
+		t.set(banner, {opacity:1});
+
+		
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Starts the animation
+	function start() {
+		tl.from(txt2a, .75, {y:"-=100", opacity:0, ease: Sine.easeOut}, "+=1.5")
+		.from(txt2b, 1, {y:"-=100", opacity:0, ease: Sine.easeOut})
+		.from(txt2c, 1, {y:"-=100", opacity:0, ease: Sine.easeOut}, "-=.25")
+		
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
