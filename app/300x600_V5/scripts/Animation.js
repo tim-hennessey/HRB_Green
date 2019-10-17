7d460a8854315aa62f316bb76c96743db54e82d4
tim-hennessey <tim.hennessey@deutschinc.com>
2019-10-16 19:19:24 -0700

    Creative Updates

--- app/300x600_V5/scripts/Animation.js
+++ app/300x600_V5/scripts/Animation.js
@@ -6,7 +6,7 @@ app.Animation = (function () {
 	var banner = document.getElementById('banner');
 	var t = TweenMax;
 	var tl = new TimelineMax();
-	var txt2a = document.getElementById('txt2a');
+	// var txt2a = document.getElementById('txt2a');
 	var txt2b = document.getElementById('txt2b');
 	var txt2c = document.getElementById('txt2c');
 	var txt2d = document.getElementById('txt2d');
@@ -24,8 +24,8 @@ app.Animation = (function () {
 	// --------------------------------------------------------------------------------------
 	// Starts the animation
 	function start() {
-		tl.from(txt2a, .75, {y:"-=100", ease: Sine.easeOut}, "+=1.5")
-		.from(txt2b, .75, {y:"-=100", ease: Sine.easeOut}, "-=.25")
+		// tl.from(txt2a, .75, {y:"-=100", ease: Sine.easeOut}, "+=1.5")
+		tl.from(txt2b, .75, {y:"-=100", ease: Sine.easeOut}, "+=1.5")
 		.from(txt2c, .75, {y:"-=100", ease: Sine.easeOut}, "-=.25")
 		.from(txt2d, .75, {y:"-=100", ease: Sine.easeOut}, "-=.25")
 		.from(txt2e, .75, {y:"-=100", ease: Sine.easeOut}, "-=.25");
